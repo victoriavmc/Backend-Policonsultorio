7fe3dfc32c9f3a1243ef8d913b45584021ad3d33
SantiAranda <134805510+SantiAranda@users.noreply.github.com>
2025-10-29 20:42:34 -0300

    Rutas desacopladas para agregar los permisos a cada uno

--- routes/api.php
+++ routes/api.php
@@ -44,76 +44,185 @@ Route::apiResource('datos-personales', DatoPersonalController::class)
 Route::apiResource('usuarios', UserController::class)->except(['store', 'destroy', 'update']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('auditorias', AuditoriasController::class);
-    Route::apiResource('indicaciones', IndicacionController::class);
-    Route::apiResource('recetas', RecetaController::class);
-    Route::apiResource('imagenes', ImagenController::class);
 
-    Route::middleware('role:medico, secretario, usuario')->group(function () {
-
+    // Auditorias
+    Route::controller(AuditoriasController::class)->group(function () {
+        Route::get('/auditorias', 'index');
+        Route::post('/auditorias', 'store');
+        Route::get('/auditorias/{id}', 'show');
+        Route::put('/auditorias/{id}', 'update');
+        Route::delete('/auditorias/{id}', 'destroy');
     });
 
-    Route::middleware('role:medico, secretario')->group(function () {
-
+    // Indicaciones
+    Route::controller(IndicacionController::class)->group(function () {
+        Route::get('/indicaciones', 'index');
+        Route::post('/indicaciones', 'store');
+        Route::get('/indicaciones/{id}', 'show');
+        Route::put('/indicaciones/{id}', 'update');
+        Route::delete('/indicaciones/{id}', 'destroy');
     });
 
-    Route::middleware('role:medico')->group(function() {
-
+    // Recetas
+    Route::controller(RecetaController::class)->group(function () {
+        Route::get('/recetas', 'index');
+        Route::post('/recetas', 'store');
+        Route::get('/recetas/{id}', 'show');
+        Route::put('/recetas/{id}', 'update');
+        Route::delete('/recetas/{id}', 'destroy');
     });
 
-    Route::middleware('role:secretario')->group(function() {
-
+    // Imagenes
+    Route::controller(ImagenController::class)->group(function () {
+        Route::get('/imagenes', 'index');
+        Route::post('/imagenes', 'store');
+        Route::get('/imagenes/{id}', 'show');
+        Route::put('/imagenes/{id}', 'update');
+        Route::delete('/imagenes/{id}', 'destroy');
     });
 
-    Route::middleware('role:usuario')->group(function() {
+    // Solicitudes
+    Route::controller(SolicitudesController::class)->group(function () {
+        Route::get('/solicitudes', 'index');
+        Route::post('/solicitudes', 'store');
+        Route::get('/solicitudes/{id}', 'show');
+        Route::put('/solicitudes/{id}', 'update');
+        Route::delete('/solicitudes/{id}', 'destroy');
+    });
 
+    // Tipos Solicitudes
+    Route::controller(TiposSolicitudesController::class)->group(function () {
+        Route::get('/tipos-solicitudes', 'index');
+        Route::post('/tipos-solicitudes', 'store');
+        Route::get('/tipos-solicitudes/{id}', 'show');
+        Route::put('/tipos-solicitudes/{id}', 'update');
+        Route::delete('/tipos-solicitudes/{id}', 'destroy');
+    });
+
+    // Seguimiento Tratamiento
+    Route::controller(SeguimientoTratamientoController::class)->group(function () {
+        Route::get('/seguimientos-tratamientos', 'index');
+        Route::post('/seguimientos-tratamientos', 'store');
+        Route::get('/seguimientos-tratamientos/{id}', 'show');
+        Route::put('/seguimientos-tratamientos/{id}', 'update');
+        Route::delete('/seguimientos-tratamientos/{id}', 'destroy');
+    });
+
+    // Obras Sociales
+    Route::controller(ObraSocialController::class)->group(function () {
+        Route::get('/obras-sociales/all', 'getAll');
+        Route::get('/obras-sociales', 'index');
+        Route::post('/obras-sociales', 'store');
+        Route::get('/obras-sociales/{id}', 'show');
+        Route::put('/obras-sociales/{id}', 'update');
+        Route::delete('/obras-sociales/{id}', 'destroy');
+    });
+
+    // Pacientes
+    Route::controller(PacienteController::class)->group(function () {
+        Route::get('/pacientes', 'index');
+        Route::post('/pacientes', 'store');
+        Route::get('/pacientes/{id}', 'show');
+        Route::put('/pacientes/{id}', 'update');
+        Route::delete('/pacientes/{id}', 'destroy');
+    });
+
+    // Formularios PDFs
+    Route::controller(FormularioPDFController::class)->group(function () {
+        Route::get('/formularios-pdfs', 'index');
+        Route::post('/formularios-pdfs', 'store');
+        Route::get('/formularios-pdfs/{id}', 'show');
+        Route::put('/formularios-pdfs/{id}', 'update');
+        Route::delete('/formularios-pdfs/{id}', 'destroy');
+    });
+
+    // Horarios
+    Route::controller(HorarioController::class)->group(function () {
+        Route::get('/horarios', 'index');
+        Route::post('/horarios', 'store');
+        Route::get('/horarios/{id}', 'show');
+        Route::put('/horarios/{id}', 'update');
+        Route::delete('/horarios/{id}', 'destroy');
+    });
+
+    // Medicos
+    Route::controller(MedicoController::class)->group(function () {
+        Route::get('/medicos', 'index');
+        Route::post('/medicos', 'store');
+        Route::get('/medicos/{id}', 'show');
+        Route::put('/medicos/{id}', 'update');
+        Route::delete('/medicos/{id}', 'destroy');
+    });
+
+    // HorariosMedicos
+    Route::controller(HorariosMedicoController::class)->group(function () {
+        Route::get('/horariosMedicos', 'index');
+        Route::post('/horariosMedicos', 'store');
+        Route::get('/horariosMedicos/{id}', 'show');
+        Route::put('/horariosMedicos/{id}', 'update');
+        Route::delete('/horariosMedicos/{id}', 'destroy');
+    });
+
+    // Seguimiento Pago
+    Route::controller(SeguimientosPagoController::class)->group(function () {
+        Route::get('/seguimientosPagos', 'index');
+        Route::post('/seguimientosPagos', 'store');
+        Route::get('/seguimientosPagos/{id}', 'show');
+        Route::put('/seguimientosPagos/{id}', 'update');
+        Route::delete('/seguimientosPagos/{id}', 'destroy');
+    });
+
+    // Tratamientos
+    Route::controller(TratamientoController::class)->group(function () {
+        Route::get('/tratamientos', 'index');
+        Route::post('/tratamientos', 'store');
+        Route::get('/tratamientos/{id}', 'show');
+        Route::put('/tratamientos/{id}', 'update');
+        Route::delete('/tratamientos/{id}', 'destroy');
+    });
+
+    // Turnos 
+    Route::controller(TurnoController::class)->group(function () {
+        Route::get('/turnos', 'index');
+        Route::post('/turnos', 'store');
+        Route::get('/turnos/{id}', 'show');
+        Route::put('/turnos/{id}', 'update');
+        Route::delete('/turnos/{id}', 'destroy');
+    });
+
+    // Diagnosticos
+    Route::controller(DiagnosticosController::class)->group(function () {
+        Route::get('/diagnosticos', 'index');
+        Route::post('/diagnosticos', 'store');
+        Route::get('/diagnosticos/{id}', 'show');
+        Route::put('/diagnosticos/{id}', 'update');
+        Route::delete('/diagnosticos/{id}', 'destroy');
+    });
+
+    // HistorialClinico
+    Route::controller(HistorialClinicoController::class)->group(function () {
+        Route::get('/historialClinicos', 'index');
+        Route::post('/historialClinicos', 'store');
+        Route::get('/historialClinicos/{id}', 'show');
+        Route::put('/historialClinicos/{id}', 'update');
+        Route::delete('/historialClinicos/{id}', 'destroy');
+    });
+
+    // Noticias
+    Route::controller(NoticiaController::class)->group(function () {
+        Route::get('/noticias', 'index');
+        Route::post('/noticias', 'store');
+        Route::get('/noticias/{id}', 'show');
+        Route::put('/noticias/{id}', 'update');
+        Route::delete('/noticias/{id}', 'destroy');
+    });
+
+    // Consultas
+    Route::controller(ConsultaController::class)->group(function () {
+        Route::get('/consultas', 'index');
+        Route::post('/consultas', 'store');
+        Route::get('/consultas/{id}', 'show');
+        Route::put('/consultas/{id}', 'update');
+        Route::delete('/consultas/{id}', 'destroy');
     });
 });
-// Solicitudes
-Route::apiResource('solicitudes', SolicitudesController::class)->middleware('role:medico');
-
-// Tipos Solicitudes
-Route::apiResource('tipos-solicitudes', TiposSolicitudesController::class);
-
-// Seguimiento Tratamiento
-Route::apiResource('seguimientos-tratamientos', SeguimientoTratamientoController::class);
-
-// Obras Sociales
-Route::get('obras-sociales/all', [ObraSocialController::class, 'getAll']);
-Route::apiResource('obras-sociales', ObraSocialController::class);
-
-// Pacientes
-Route::apiResource('pacientes', PacienteController::class);
-
-// Formularios PDFs
-Route::apiResource('formularios-pdfs', FormularioPDFController::class);
-
-// Horarios
-Route::apiResource('horarios', HorarioController::class);
-
-// Medicos
-Route::apiResource('medicos', MedicoController::class);
-
-// HorariosMedicos
-Route::apiResource('horariosMedicos', HorariosMedicoController::class);
-
-// Seguimiento Pago
-Route::apiResource('seguimientosPagos', SeguimientosPagoController::class);
-
-// Tratamientos
-Route::apiResource('tratamientos', TratamientoController::class);
-
-// Turnos 
-Route::apiResource('turnos', TurnoController::class);
-
-// Diagnosticos
-Route::apiResource('diagnosticos', DiagnosticosController::class);
-
-// HistorialClinico
-Route::apiResource('historialClinicos', HistorialClinicoController::class);
-
-// Noticias
-Route::apiResource('noticias', NoticiaController::class);
-
-// Consultas
-Route::apiResource('consultas', ConsultaController::class);
